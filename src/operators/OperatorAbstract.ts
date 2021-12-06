0ca206a7c2668054e01ac60cbe07ac83bdfb451c
Spacexion <xionteamionx@gmail.com>
2021-12-06 07:46:41 +0100

    Add basic src files

---
+++ src/operators/OperatorAbstract.ts
@@ -0,0 +1,11 @@
+
+
+export class OperatorAbstract {
+  name: string;
+  symbol: string;
+
+  constructor(name: string, symbol: string) {
+   this.name = name;
+   this.symbol = symbol;
+  }
+}
---
+++ src/operators/Sin.operator.ts
@@ -0,0 +1,11 @@
+import {OperatorAbstract} from "./OperatorAbstract";
+import {OperatorInterface} from "./OperatorInterface";
+
+
+export class Sin extends OperatorAbstract implements OperatorInterface {
+
+  exec(): number {
+    return 0;
+  }
+
+}
