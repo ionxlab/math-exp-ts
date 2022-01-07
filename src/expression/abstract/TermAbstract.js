166bc9d3a2f64a42151284cdbc3c63d51fcc9559
Spacexion <xionteamionx@gmail.com>
2022-01-07 07:52:32 +0100

    Remove useless js files.

--- src/expression/abstract/TermAbstract.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.TermAbstract = void 0;
-var TermAbstract = /** @class */ (function () {
-    function TermAbstract() {
-    }
-    return TermAbstract;
-}());
-exports.TermAbstract = TermAbstract;
